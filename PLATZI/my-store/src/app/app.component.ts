cd38fb21bc4aec1d006b9704f6801f8331ec2bf3
echicasprado <everchicasp@outlook.com>
2023-01-17 16:27:10 -0600

    Used output values from child to parent

--- PLATZI/my-store/src/app/app.component.ts
+++ PLATZI/my-store/src/app/app.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'my-store';
   url:String = 'https://static.platzi.com/media/achievements/badge-angular-componentes-servicios-75f68ec3-48e0-430e-b7e2-889fad0d1984.png';
+
+  onLoaded(url:String) {
+    console.log('loaded parent',url)
+  }
+
+
 }
--- PLATZI/my-store/src/app/components/img/img.component.ts
+++ PLATZI/my-store/src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -6,6 +6,19 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./img.component.scss']
 })
 export class ImgComponent {
+
   @Input() imgSrc:String = '';
-  defaultValue:String = "No se encuentra image disponible";
+  @Output() loaded = new EventEmitter();
+  defaultValue:String = './../assets/images/defaultImage.png';
+
+
+  imgError() {
+    this.imgSrc = this.defaultValue;
+  }
+
+  imgLoaded(){
+    console.log("loaded child");
+    this.loaded.emit(this.imgSrc)
+  }
+
 }
